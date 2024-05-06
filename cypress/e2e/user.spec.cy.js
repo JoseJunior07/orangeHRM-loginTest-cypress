8f5297c5a6df78fd0dce8ff279f98fa52c2f30cd
José Júnior <josecdcjunior@gmail.com>
2024-05-06 15:50:22 -0300

    Add page objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,50 +1,52 @@
 import userData from '../fixtures/users/user-data.json'
+import LoginPage from '../pages/loginPage.js'
+import DashboardPage from '../pages/dashboardPage.js'
+import MenuPage from '../pages/menuPage.js'
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPage = new MenuPage()
 
 describe('Orange HRM Tests', () => {
 
   const selectorsList = {
-    usernameField: '[name="username"]',
-    passwordField: '[name="password"]',
-    loginButton: '[type="submit"]',
-    sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
-    wrongCredentialAlert: '[role="alert"]',
-    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+
+    
     firstNameField: '[name="firstName"]',
     lastNameField: '[name="lastName"]',
     genericField: '.oxd-input--active',
     dateField: '[placeholder="yyyy-dd-mm"]',
     dateCloseButton: '.--close',
-    nationalityField: '[tabindex="0"]',
-    maritalStatusField: '[tabindex=0"]',
+    genericComboBox: '.oxd-select-text--arrow',
+    secondItemComboBox: '.oxd-select-dropdown > :nth-child(26)',
+    thirdItemComboBox: '.oxd-select-dropdown > :nth-child(2)',
     submitButton: '[type="submit"]'
   }
 
 
   it.only('User Info Update - Success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
-    cy.get(selectorsList.myInfoButton).click()
-    cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
-    cy.get(selectorsList.lastNameField).clear().type('lastNameTest')
-    cy.get(selectorsList.genericField).eq(3).clear().type('EmployeeId')
-    cy.get(selectorsList.genericField).eq(4).clear().type('OtherIdTest')
-    cy.get(selectorsList.genericField).eq(5).clear().type('LicenseNumber')
-    cy.get(selectorsList.dateField).eq(0).clear().type('2025-05-02')
-    cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.nationalityField).eq(0).click()
-    cy.contains(selectorsList.nationalityOption, 'Brazilian').click()
-    cy.get(selectorsList.nationalityField).eq(1).click()
-    cy.contains(selectorsList.nationalityOption, 'Married').click()
-    cy.get(selectorsList.submitButton).eq(0).click()
-    cy.get('body').should('contain', 'Successfully Updated')
-    cy.get('.oxd-toast-close').click()
+    loginPage.accessLoginPage()
+    loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
+
+    dashboardPage.checkDashboardPage()
+
+    menuPage.accessMyInfo()
 
     
-    
+    // cy.get(selectorsList.myInfoButton).click()
+    // cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
+    // cy.get(selectorsList.lastNameField).clear().type('lastNameTest')
+    // cy.get(selectorsList.genericField).eq(3).clear().type('EmployeeId')
+    // cy.get(selectorsList.genericField).eq(4).clear().type('OtherIdTest')
+    // cy.get(selectorsList.genericField).eq(5).clear().type('LicenseNumber')
+    // cy.get(selectorsList.dateField).eq(0).clear().type('2025-05-02')
+    // cy.get(selectorsList.dateCloseButton).click()
+    // cy.get(selectorsList.genericComboBox).eq(0).click()
+    // cy.get(selectorsList.secondItemComboBox).click()
+    // cy.get(selectorsList.genericComboBox).eq(1).click()
+    // cy.get(selectorsList.thirdItemComboBox).click()
+    // cy.get(selectorsList.submitButton).eq(0).click()
+    // cy.get('body').should('contain', 'Successfully Updated') 
   })
   
   it('Login - Fail', () => {
